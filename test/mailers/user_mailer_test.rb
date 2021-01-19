9a50981c30ed57f7218028a30d3ebe92de66f568
piyoppa <piyoppa@gmail.com>
2021-01-19 18:20:38 +0900

    add test

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class UserMailerTest < ActionMailer::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "welcome" do
+    email = UserMailer.with(to: "piyopiyo@example.com", name: "piyopiyo").welcome
+    assert_emails(1){ email.deliver_now }
+    assert_equal ["perfect_rails@example.com"], email.from
+    assert_equal ["piyopiyo@example.com"], email.to
+    assert_equal "登録完了", email.subject
+    assert_includes email.text_part.decoded, "piyopiyo"
+    assert_includes email.html_part.decoded, "piyopiyo"
+  end
 end
